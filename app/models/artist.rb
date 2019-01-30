d77908a6beb9f77ee85c7bfe0f1d26dc89f43ea7
Luis Herrera <herrera.lrh@gmail.com>
2019-01-30 19:26:01 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    artist_songs = self.songs
-    artist_songs.genre.count
+    self.genres.count
   end
 end
